8701340fd4b68d5406c86e2fa70cfead01fa1e6e
Maximilian <scientificresearch309@gmail.com>
2023-04-10 14:29:13 +0200

    All headers (h1,h2,h3) have the same color!

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
     <div className="App">
       <h1>Info Site</h1>
       <p>Welcome to this Site!</p>
+      <h2>Detailed Info</h2>
+      <p>kjsdghjgsddkjfhkjsdkfshdfkjshd</p>
+      <h3>More Info</h3>
+      <p>kjsdghjgsddkjfhkjsdkfshdfkjshd</p>
     </div>
   );
 }
