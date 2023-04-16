0e53c7eac69fda2090f7b819af01598172cb478b
Maximilian <scientificresearch309@gmail.com>
2023-04-16 21:54:27 +0200

    list and @for works but it must be outside of content in App.scss because in App.tsx, they are out of content.

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,14 @@ function App() {
         <button className="error error-disabled">Error</button>
         <button className="info info-disabled">Info</button>
       </section>
+
+      <hr />
+      <h1 className="special">The H1</h1>
+      <h2 className="special">The H2</h2>
+      <h3 className="special">The H3</h3>
+      <h4 className="special">The H4</h4>
+      <h5 className="special">The H5</h5>
+      <h6 className="special">The H6</h6>
     </div>
   );
 }
